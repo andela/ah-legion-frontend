5ea24cec2f6613f73e5742e22f87c3908ee8de02
Rachel Maina <rachel.maina@andela.com>
2019-04-02 14:41:07 +0300

    feature(react router): Setup react router version 4  - add react-router-dom  - add react-bootstrap  - add components, routes and css folders  - add Header, Footer, Login and Home components  - add routing
    [Finishes #164046205]

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer">
+    <hr />
+    <p>Author&apos;s Haven. All rights reserved. &copy; 2019</p>
+  </div>
+);
+export default Footer;
--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import App from '../../components/App';
-describe('App', () => {
+import Home from '../../components/Home';
+
+describe('Home', () => {
   it('div tag displays correctly in "debug" mode', () => {
-    const component = shallow(<App debug />);
+    const component = shallow(<Home debug />);
     expect(component.find('div')).toBeDefined();
   
   });
   it('H3 tag displays correctly in "debug" mode', () => {
-    const component = shallow(<App debug />);
+    const component = shallow(<Home debug />);
     expect(component.find('div.h3')).toBeDefined();
   
   });
