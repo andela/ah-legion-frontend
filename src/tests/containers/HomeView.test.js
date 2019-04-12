092488b4bdce4895fa299313798a4ba9c121ed47
Rachel Maina <rachel.maina@andela.com>
2019-04-12 09:52:15 +0300

    feature(create article):user can create an article  - add create article components, container, actions and actiontypes  - add updating while user types  - add article publishing  - add pretected routing  - add tests
    [Finishes #165299388]

--- src/tests/containers/HomeView.test.js
+++ src/tests/containers/HomeView.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import HomeView, { mapDispatchToProps } from '../../containers/HomeView';
 
-
 describe('Home View  container', () => {
   it("Should have correct state", () => {
     const component = shallow(<HomeView />);
     expect(component.length).toBe(1);
   });
 });
+ 
